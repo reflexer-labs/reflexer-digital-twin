fb2549bcacdb0457a5e583a7719090edf6d068f3
Benjamin Scholtz <ben@bitsofether.com>
2020-08-23 14:57:20 +0200

    Refactor directory layout

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from cadCAD.engine import ExecutionMode, ExecutionContext, Executor
 
-from pi_controller import config
+from experiment import config
 
 from cadCAD import configs
 import pandas as pd
