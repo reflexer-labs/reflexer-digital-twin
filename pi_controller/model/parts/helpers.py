a36238b421c7d6d8c1e8be94b6e637979b381a6b
Benjamin Scholtz <ben@bitsofether.com>
2020-08-23 14:52:51 +0200

    Remove unused import

--- pi_controller/model/parts/helpers.py
+++
@@ -1,7 +0,0 @@
-def did_deviation_update(error, latest_devation_type):
-    if (error >= 0 and latest_devation_type == -1):
-        return False
-    elif (error < 0 and latest_devation_type == 1):
-        return False
-    else:
-        return True
