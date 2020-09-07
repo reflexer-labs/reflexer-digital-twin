30af258addbc48dbb3355962aaacdefe6be24c18
Benjamin Scholtz <ben@bitsofether.com>
2020-09-07 19:55:25 +0200

    Update script run

--- models/v1/run.py
+++ models/v1/run.py
@@ -2,19 +2,9 @@ from cadCAD.engine import ExecutionMode, ExecutionContext, Executor
 from config import Config
 
 import pandas as pd
-import copy
 
-debt_price_source_file = './test/data/debt-price-test-data.csv'
-debt_price_dataframe = pd.read_csv(debt_price_source_file)
 
-SIMULATION_TIMESTEPS = range(debt_price_dataframe.shape[0])
-
-env_processes = {
-    'seconds_passed': lambda state, _sweep, _value, df=debt_price_dataframe.copy(): int(df.iloc[state['timestep'] - 1]['seconds_passed']),
-    'price_move': lambda state, _sweep, _value, df=debt_price_dataframe.copy(): float(df.iloc[state['timestep'] - 1]['price_move']),
-}
-
-def run(clear_configs: bool=False, drop_midsteps: bool=True, config: Config=Config(T=SIMULATION_TIMESTEPS, env_processes=env_processes)) -> pd.DataFrame:
+def run(clear_configs: bool=False, drop_midsteps: bool=True, config: Config=Config()) -> pd.DataFrame:
     configs = config.append(clear_configs=clear_configs)
 
     exec_mode = ExecutionMode()
@@ -35,5 +25,17 @@ def run(clear_configs: bool=False, drop_midsteps: bool=True, config: Config=Conf
     return df.reset_index()
 
 if __name__ == '__main__':
-    results = run(True)
+    debt_price_source_file = './test/data/debt-price-test-data.csv'
+    debt_price_dataframe = pd.read_csv(debt_price_source_file)
+
+    SIMULATION_TIMESTEPS = range(debt_price_dataframe.shape[0])
+
+    env_processes = {
+        'seconds_passed': lambda state, _sweep, _value, df=debt_price_dataframe.copy(): int(df.iloc[state['timestep'] - 1]['seconds_passed']),
+        'price_move': lambda state, _sweep, _value, df=debt_price_dataframe.copy(): float(df.iloc[state['timestep'] - 1]['price_move']),
+    }
+
+    config = Config(T=SIMULATION_TIMESTEPS, env_processes=env_processes)
+
+    results = run(drop_midsteps=True, config=config)
     print(results)
