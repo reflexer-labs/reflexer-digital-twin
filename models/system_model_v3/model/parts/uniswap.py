50f789e4e5250d4cf8e9dd8ec2a29000d4c1dcef
BenSchZA <ben@bitsofether.com>
2021-01-27 10:52:06 +0200

    Update Uniswap docs

--- models/system_model_v3/model/parts/uniswap.py
+++ models/system_model_v3/model/parts/uniswap.py
@@ -17,6 +17,12 @@ def update_UNI_supply(params, substep, state_history, state, policy_input):
 # See https://github.com/runtimeverification/verified-smart-contracts/blob/uniswap/uniswap/x-y-k.pdf for original v1 specification
 
 def add_liquidity(reserve_balance, supply_balance, voucher_balance, tokens, value):
+    '''
+    Example:
+    new_reserve = (1 + alpha)*reserve_balance
+    new_supply = (1 + alpha)*supply_balance
+    new_vouchers = (1 + alpha)*voucher_balance
+    '''
     if voucher_balance <= 0:        
         dr = value
         ds = tokens
@@ -29,27 +35,31 @@ def add_liquidity(reserve_balance, supply_balance, voucher_balance, tokens, valu
     ds = alpha*supply_balance
     dv = alpha*voucher_balance
     
-    #new_reserve = (1 + alpha)*reserve_balance
-    #new_supply = (1 + alpha)*supply_balance
-    #new_vouchers = (1 + alpha)*voucher_balance
-    
     return (dr, ds, dv)
 
 def remove_liquidity(reserve_balance, supply_balance, voucher_balance, tokens):
+    '''
+    Example:
+    new_reserve = (1 - alpha)*reserve_balance
+    new_supply = (1 - alpha)*supply_balance
+    new_liquidity_tokens = (1 - alpha)*liquidity_token_balance
+    '''
     alpha = tokens/voucher_balance
     
     dr = -alpha*reserve_balance
     ds = -alpha*supply_balance
     dv = -alpha*voucher_balance
     
-    #new_reserve = (1 - alpha)*reserve_balance
-    #new_supply = (1 - alpha)*supply_balance
-    #new_liquidity_tokens = (1 - alpha)*liquidity_token_balance
-    
     return (dr, ds, dv)
 
-# How much y received for selling dx?
 def get_input_price(dx, x_balance, y_balance, trade_fee=0.01):
+    '''
+    How much y received for selling dx?
+
+    Example:
+    new_x = (1 + alpha)*x_balance
+    new_y = y_balance - dy
+    '''
     rho = trade_fee
     
     alpha = dx / x_balance
@@ -60,14 +70,16 @@ def get_input_price(dx, x_balance, y_balance, trade_fee=0.01):
     _dx = alpha * x_balance
     _dy = -dy
     
-    # Example:
-    # new_x = (1 + alpha)*x_balance
-    # new_y = y_balance - dy
-    
     return (_dx, _dy)
 
-# How much x needs to be sold to buy dy?
 def get_output_price(dy, x_balance, y_balance, trade_fee=0.01):
+    '''
+    How much x needs to be sold to buy dy?
+
+    Example:
+    new_x = x_balance + dx
+    new_y = (1 - beta)*y_balance
+    '''
     rho = trade_fee
     
     beta = dy / y_balance
@@ -78,28 +90,31 @@ def get_output_price(dy, x_balance, y_balance, trade_fee=0.01):
     _dx = dx
     _dy = -beta * y_balance
     
-    # Example:
-    # new_x = x_balance + dx
-    # new_y = (1 - beta)*y_balance
-    
     return (_dx, _dy)
 
 # Token trading
 def collateral_to_token(value, reserve_balance, supply_balance, trade_fee):
+    '''
+    Trade collateral for token
+
+    Example:
+    new_reserve = reserve_balance + dx
+    new_supply = supply_balance - dy
+    '''
     if reserve_balance == 0: return 0
     dx,dy = get_input_price(value, reserve_balance, supply_balance, trade_fee)
-    
-    #new_reserve = reserve_balance + dx
-    #new_supply = supply_balance - dy
     
     return abs(dy)
 
 def token_to_collateral(tokens, reserve_balance, supply_balance, trade_fee):
+    '''
+    Trade token for collateral
+
+    Example:
+    new_reserve = reserve_balance - dx
+    new_supply = supply_balance + dy
+    '''
     if supply_balance == 0: return 0
     dx,dy = get_input_price(tokens, supply_balance, reserve_balance, trade_fee)
-    
-    # Example:
-    # new_reserve = reserve_balance - dx
-    # new_supply = supply_balance + dy
     
     return abs(dy)
