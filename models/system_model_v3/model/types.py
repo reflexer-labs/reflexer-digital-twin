d8576178ee24cc25520bd5d84051962157674016
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-03-16 11:39:37 -0300

    add types file

---
+++ models/system_model_v3/model/types.py
@@ -0,0 +1,31 @@
+from typing import Dict, TypedDict, List
+
+
+Seconds = int
+Height = int
+ETH = float 
+USD = float
+RAI = float
+UNI = float
+ETH_per_USD = float
+RAI_per_USD = float
+USD_per_Seconds = float
+Percentage_Per_Second = float
+Percentage = float
+Per_USD: float
+Per_USD_Seconds: float
+Per_RAY: float
+
+class CDP_Metric(TypedDict):
+    cdp_count: int
+    open_cdp_count: int
+    closed_cdp_count: int
+    mean_cdp_collateral: float
+    median_cdp_collateral: float
+
+
+class OptimalValues(TypedDict):
+    u_1: RAI
+    u_2: RAI
+    v_1: RAI
+    v_2: RAI
