b73f9a4ae2fe2318140ffed218288b732c70c391
Danillo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-05-20 15:53:03 -0300

    automatically get fit_shape and fit_scale

--- rai_digital_twin/stochastic_fit.py
+++ rai_digital_twin/stochastic_fit.py
@@ -2,14 +2,16 @@
 
 import pandas as pd
 import numpy as np
+from dataclasses import dataclass
 from scipy.stats import gamma
 
-def kalman_filter(observations,initialValue,truthValues=None,plot=False,paramExport=False):
+
+def kalman_filter(observations, initialValue, truthValues=None, plot=False, paramExport=False):
     '''
     Description:
     Function to create a Kalman Filter for smoothing currency timestamps in order to search for the
     intrinisic value.
-    
+
     Parameters:
     observations: Array of observations, i.e. predicted secondary market prices.
     initialValue: Initial Starting value of filter
@@ -17,67 +19,68 @@ def kalman_filter(observations,initialValue,truthValues=None,plot=False,paramExp
     blank if none exist
     plot: If True, plot the observations, truth values and kalman filter.
     paramExport: If True, the parameters xhat,P,xhatminus,Pminus,K are returned to use in training.
-    
+
     Example:
     xhat,P,xhatminus,Pminus,K = kalman_filter(observations=train.Close.values[0:-1],
                                               initialValue=train.Close.values[-1],paramExport=True)
     '''
     # intial parameters
     n_iter = len(observations)
-    sz = (n_iter,) # size of array
-    if isinstance(truthValues,np.ndarray):
-        x = truthValues # truth value
-    z = observations# observations (normal about x, sigma=0.1)
+    sz = (n_iter,)  # size of array
+    if isinstance(truthValues, np.ndarray):
+        x = truthValues  # truth value
+    z = observations  # observations (normal about x, sigma=0.1)
 
-    Q = 1e-5 # process variance
+    Q = 1e-5  # process variance
 
     # allocate space for arrays
-    xhat=np.zeros(sz)      # a posteri estimate of x
-    P=np.zeros(sz)         # a posteri error estimate
-    xhatminus=np.zeros(sz) # a priori estimate of x
-    Pminus=np.zeros(sz)    # a priori error estimate
-    K=np.zeros(sz)         # gain or blending factor
+    xhat = np.zeros(sz)      # a posteri estimate of x
+    P = np.zeros(sz)         # a posteri error estimate
+    xhatminus = np.zeros(sz)  # a priori estimate of x
+    Pminus = np.zeros(sz)    # a priori error estimate
+    K = np.zeros(sz)         # gain or blending factor
 
-    R = 0.1**2 # estimate of measurement variance, change to see effect
+    R = 0.1**2  # estimate of measurement variance, change to see effect
 
     # intial guesses
     xhat[0] = initialValue
     P[0] = 1.0
 
-    for k in range(1,n_iter):
+    for k in range(1, n_iter):
         # time update
         xhatminus[k] = xhat[k-1]
         Pminus[k] = P[k-1]+Q
 
         # measurement update
-        K[k] = Pminus[k]/( Pminus[k]+R )
+        K[k] = Pminus[k]/(Pminus[k]+R)
         xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
         P[k] = (1-K[k])*Pminus[k]
 
-    if plot==True:
+    if plot == True:
         plt.figure()
-        plt.plot(z,'k+',label='Actual data')
-        plt.plot(xhat,'b-',label='a posteri estimate')
-        if isinstance(truthValues,np.ndarray):
-            plt.plot(x,color='g',label='truth value')
+        plt.plot(z, 'k+', label='Actual data')
+        plt.plot(xhat, 'b-', label='a posteri estimate')
+        if isinstance(truthValues, np.ndarray):
+            plt.plot(x, color='g', label='truth value')
         plt.legend()
         plt.title('Kalman Filter Estimates', fontweight='bold')
         plt.xlabel('Iteration')
         plt.ylabel('USD')
         plt.show()
 
-    if paramExport==True:
-        return  xhat,P,xhatminus,Pminus,K
+    if paramExport == True:
+        return xhat, P, xhatminus, Pminus, K
 
     else:
 
         return xhat
 
-def kalman_filter_predict(xhat,P,xhatminus,Pminus,K,observations,truthValues=None,paramExport=False):
+
+def kalman_filter_predict(xhat, P, xhatminus, Pminus, K, observations, truthValues=None, paramExport=False):
     '''
     Description:
     Function to predict a pre-trained Kalman Filter 1 step forward.
-    
+
     Parameters:
     xhat: Trained Kalman filter values - array
     P: Trained Kalman variance - array
@@ -88,41 +91,89 @@ def kalman_filter_predict(xhat,P,xhatminus,Pminus,K,observations,truthValues=Non
     truthValues: Array of truth values, i.e. GPS location or secondary market prices. Or can be left
     blank if none exist
     paramExport: If True, the parameters xhat,P,xhatminus,Pminus,K are returned to use in next predicted step.
-    
+
     Example:
     xhat,P,xhatminus,Pminus,K = kalman_filter_predict(xhatInput,PInput,
                                                       xhatminusInput,PminusInput,KInput,observation,
                                                        paramExport=True)
     '''
     # intial parameters
-    if isinstance(truthValues,np.ndarray):
-        x = truthValues # truth value
-    z = observations# observations (normal about x, sigma=0.1)
+    if isinstance(truthValues, np.ndarray):
+        x = truthValues  # truth value
+    z = observations  # observations (normal about x, sigma=0.1)
 
-    Q = 1e-5 # process variance
+    Q = 1e-5  # process variance
 
-    R = 0.1**2 # estimate of measurement variance, change to see effect
-
+    R = 0.1**2  # estimate of measurement variance, change to see effect
 
     # time update
-    xhatminus = np.append(xhatminus,xhat[-1])
-    Pminus = np.append(Pminus,P[-1]+Q)
+    xhatminus = np.append(xhatminus, xhat[-1])
+    Pminus = np.append(Pminus, P[-1]+Q)
 
     # measurement update
-    K = np.append(K,Pminus[-1]/( Pminus[-1]+R ))
-    xhat = np.append(xhat,xhatminus[-1]+K[-1]*(z[-1]-xhatminus[-1]))
-    P = np.append(P,(1-K[-1])*Pminus[-1])
+    K = np.append(K, Pminus[-1]/(Pminus[-1]+R))
+    xhat = np.append(xhat, xhatminus[-1]+K[-1]*(z[-1]-xhatminus[-1]))
+    P = np.append(P, (1-K[-1])*Pminus[-1])
 
-    if paramExport==True:
-        return  xhat,P,xhatminus,Pminus,K
+    if paramExport == True:
+        return xhat, P, xhatminus, Pminus, K
 
     else:
 
         return xhat
 
 
+def generate_eth_timeseries(xhat, P, xhatminus, Pminus, K):
+    eth_values = []
+    filter_values = {'xhat':xhat,'P':P,
+                     'xhatminus':xhatminus,'Pminus':Pminus,
+                     'K':K}
 
-def fit_predict_eth_price(X: np.array, 
-                         timesteps: int,
-                         samples: int) -> np.array:
+    for i in range(0,timesteps+1):
+        sample = np.random.gamma(fit_shape, fit_scale, 1)[0]
+        eth_values.append(sample)
+        xhat,P,xhatminus,Pminus,K = kalman_filter_predict(filter_values['xhat'],
+                                                      filter_values['P'],
+                                                      filter_values['xhatminus'],
+                                                      filter_values['Pminus'],
+                                                      filter_values['K'],
+                                                      eth_values,
+                                                      paramExport=True)
+        filter_values = {'xhat':xhat,'P':P,
+                     'xhatminus':xhatminus,'Pminus':Pminus,
+                     'K':K}
+
+    return eth_values, xhat, P, xhatminus, Pminus, K
+
+@dataclass
+class StochasticParams():
+    xhat: float
+    P: float
+    xhatminus: float
+    Pminus: float
+    K: float
+
+
+def fit_eth_price(X: list(float)) -> StochasticParams:
+    results = kalman_filter(observations=X[:-1],
+                            initialValue=X.values[-1],
+                            paramExport=True,
+                            plot=True)
+
+    fit_params = StochasticParams(*results)
+    return fit_params
+
+
+def predict_eth_price(params: StochasticParams,
+                      timesteps: int,
+                      samples: int) -> list[list[float]]:
+
+    for sample in range(samples):
+        for timestep in range(timesteps):
+    pass
+
+
+def fit_predict_eth_price(X: np.array,
+                          timesteps: int,
+                          samples: int) -> np.array:
     pass
